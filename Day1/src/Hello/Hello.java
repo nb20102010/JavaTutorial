dfcd7f97a5f1044a947a4d4c3753ec9ac22d2f01
u0879939RunLi <u0879939@utah.edu>
2018-08-26 09:41:24 -0600

    Finished Lecture 2

---
+++ Day1/src/Hello/Hello.java
@@ -0,0 +1,32 @@
+package Hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("Hello, World");
+		
+//		int a = 10;
+//		int b = 20;
+//		int result = a+b;
+//		
+//		System.out.println("½á¹ûÊÇ"+result);
+		
+//		char char1 = 'a' + 28;
+//		System.out.println(char1);
+		
+//		int a = 23;
+//		a = 45;
+//		
+//		System.out.println("a="+a);
+		
+//		int a = (int)1.2;
+//		int b = (int)1.9;
+//		System.out.println(a+b);
+
+		
+		
+		
+	}
+
+}
