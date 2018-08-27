0a7a6926e52495e134106601a5b7a1be373a2b46
A8NLJZZ <A8NLJZZ@W0191270.mmm.com>
2018-08-27 12:56:34 -0600

    Finished Lecture 3

---
+++ Day1/src/Demo/Demo2.java
@@ -0,0 +1,15 @@
+package Demo;
+
+public class Demo2 {
+	
+	public static void main(String[] args) {
+		int a = 12;
+		if(a > 18) {
+			System.out.println("a > 18");
+		} else if(a > 10 && a <= 18) {
+			System.out.println("a > 10 && a <= 18");
+		} else {
+			System.out.println("a <= 10");
+		}
+	}
+}
